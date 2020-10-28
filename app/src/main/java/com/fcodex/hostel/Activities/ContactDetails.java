d5be151ae63109f1f3ee522160459452628e4ab0
asadghaffar <38716939+asadghaffar@users.noreply.github.com>
2020-10-29 03:33:09 +0500

    Google Map working

--- app/src/main/java/com/fcodex/hostel/Activities/ContactDetails.java
+++ app/src/main/java/com/fcodex/hostel/Activities/ContactDetails.java
@@ -2,17 +2,27 @@ package com.fcodex.hostel.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fcodex.hostel.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ContactDetails extends AppCompatActivity {
 
-    private Button submit;
+    private MaterialButton submit;
+    private TextView hostelName;
+    private TextView hostelAddress;
+    private TextInputEditText enterName;
+    private TextInputEditText enterEmail;
+    private TextInputEditText enterNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +34,13 @@ public class ContactDetails extends AppCompatActivity {
 
     }
 
+
     private void id() {
+        hostelName = findViewById(R.id.hostelName);
+        hostelAddress = findViewById(R.id.hostelAddress);
+        enterName = findViewById(R.id.enterName);
+        enterEmail = findViewById(R.id.enterEmail);
+        enterNumber = findViewById(R.id.enterNumber);
         submit = findViewById(R.id.submit);
     }
 
@@ -38,5 +54,27 @@ public class ContactDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String stringHostelName = hostelName.getText().toString().trim();
+                String stringHostelAddress = hostelAddress.getText().toString().trim();
+                String stringEnterName = enterName.getText().toString().trim();
+                String stringEnterEmail = enterEmail.getText().toString().trim();
+                String stringEnterNumber = enterNumber.getText().toString().trim();
+
+                if (stringEnterName.isEmpty()) {
+                    enterName.setError("Enter Name");
+                } else if (stringEnterEmail.isEmpty()) {
+                    enterEmail.setError("Enter Email");
+                } else if (stringEnterNumber.isEmpty()) {
+                    enterNumber.setError("Enter Number");
+                } else {
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/fcodex/hostel/Activities/HostelDetailedActivity.java
+++ app/src/main/java/com/fcodex/hostel/Activities/HostelDetailedActivity.java
@@ -1,11 +1,19 @@
 package com.fcodex.hostel.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.fcodex.hostel.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,8 +26,11 @@ import com.google.android.material.button.MaterialButton;
 
 public class HostelDetailedActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final int PERMISSION_REQUEST_CODE = 9001;
+    private boolean mLocationPermissionGranted;
     private GoogleMap mMap;
     private MaterialButton materialButton;
+    private String location = "Mark location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +39,27 @@ public class HostelDetailedActivity extends FragmentActivity implements OnMapRea
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
 
         id();
         onClick();
+        permision();
 
+    }
 
+    private void permision() {
+        if(mLocationPermissionGranted){
+            Toast.makeText(this, "Ready to Map!", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+                }
+            }
+        }
     }
 
     private void onClick() {
@@ -53,14 +78,29 @@ public class HostelDetailedActivity extends FragmentActivity implements OnMapRea
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(31.520441575636823, 74.32235530595678);
         mMap.addMarker(new MarkerOptions()
                 .position(sydney)
-                .title("Marker in Sydney"));
+                .title(location));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f));
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            mLocationPermissionGranted=true;
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+         }else{
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
